32fdeb1b4dcc7ce99d3bcad44ddf961fccd3883d
Roberto Fuentes <robertofuentes@MacBook-Pro-de-Roberto.local>
2020-10-06 18:24:16 -0300

    pequeños cambios

--- data/dia7/script.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-function convert_all_to_mp4() {
-  for file in *.MOV ; do
-    local bname=$(basename "$file" .MOV)
-    local mp4name="$bname.mp4"
-    ffmpeg -i "$file" -an "$mp4name"
-  done
-}
-convert_all_to_mp4
